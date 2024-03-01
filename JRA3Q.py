0e933b48588713fa84a142844de3caa1133f3eb6
Horita <ryohorita105@gmail.com>
2024-03-01 22:30:56 +0900

    Update JRA3Q.py

--- JRA3Q.py
+++ JRA3Q.py
@@ -141,7 +141,7 @@ dss['lon'].attrs['units'] = 'degrees_east'
 dss = dss.metpy.parse_cf()
 
 # 海面気圧
-dss['prmsl'] = (["lat", "lon"], gaussian_filter(dss['prmsl'].values, sigma=2.0) * units(elem_units[3]))
+#dss['prmsl'] = (["lat", "lon"], gaussian_filter(dss['prmsl'].values, sigma=2.0) * units(elem_units[3]))
 
 ##! 読み込むの高度上限の指定：tagLpより下層の等圧面データをXarray Dataset化する
 tagLp = 300
