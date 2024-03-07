1ecc14734011770817d6b258eaee45f6f539e433
Horita <ryohorita105@gmail.com>
2024-03-07 19:58:54 +0900

    Update test.py

--- test.py
+++ test.py
@@ -144,10 +144,10 @@ u = gaussian_filter(dss['10u'].values, sigma=1)
 v = gaussian_filter(dss['10v'].values, sigma=1)
 
 # wが5以下の場所のみフィルタリング
-dss['prmsl'] = (["lat", "lon"], np.where(u**2 + v**2 <= 2.5, gaussian_filter(dss['prmsl'].values, sigma=4.0), dss['prmsl'].values) * units(elem_units[3]))
-dss['prmsl'] = (["lat", "lon"], np.where(u**2 + v**2 <= 5.0, gaussian_filter(dss['prmsl'].values, sigma=4.0), dss['prmsl'].values) * units(elem_units[3]))
-dss['prmsl'] = (["lat", "lon"], np.where(u**2 + v**2 <= 10, gaussian_filter(dss['prmsl'].values, sigma=4.0), dss['prmsl'].values) * units(elem_units[3]))
-#dss['prmsl'] = (["lat", "lon"], gaussian_filter(dss['prmsl'].values, sigma=1) * units(elem_units[3]))
+#dss['prmsl'] = (["lat", "lon"], np.where(u**2 + v**2 <= 2.5, gaussian_filter(dss['prmsl'].values, sigma=4.0), dss['prmsl'].values) * units(elem_units[3]))
+#dss['prmsl'] = (["lat", "lon"], np.where(u**2 + v**2 <= 5.0, gaussian_filter(dss['prmsl'].values, sigma=4.0), dss['prmsl'].values) * units(elem_units[3]))
+#dss['prmsl'] = (["lat", "lon"], np.where(u**2 + v**2 <= 10, gaussian_filter(dss['prmsl'].values, sigma=4.0), dss['prmsl'].values) * units(elem_units[3]))
+dss['prmsl'] = (["lat", "lon"], gaussian_filter(dss['prmsl'].values, sigma=0.25) * units(elem_units[3]))
 
 ## 読み込むの高度上限の指定：tagLpより下層の等圧面データをXarray Dataset化する
 tagLp = 300
