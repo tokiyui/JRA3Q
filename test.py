6f2e95e3eab8178f66b6e3bc1fef04f6117a2f9d
Horita <ryohorita105@gmail.com>
2024-03-14 18:10:15 +0900

    Update test.py

--- test.py
+++ test.py
@@ -143,7 +143,7 @@ dss = dss.metpy.parse_cf()
 w = gaussian_filter(np.sqrt(dss['10u'].values ** 2 + dss['10v'].values ** 2), sigma=4)
 
 # wが5以下の場所のみフィルタリング
-dss['prmsl'] = (["lat", "lon"], np.where(w <= 5, gaussian_filter(dss['prmsl'].values, sigma=4), dss['prmsl'].values) * units(elem_units[3]))
+#dss['prmsl'] = (["lat", "lon"], np.where(w <= 5, gaussian_filter(dss['prmsl'].values, sigma=4), dss['prmsl'].values) * units(elem_units[3]))
 dss['prmsl'] = (["lat", "lon"], np.where(w <= 10, gaussian_filter(dss['prmsl'].values, sigma=4), dss['prmsl'].values) * units(elem_units[3]))
 dss['prmsl'] = (["lat", "lon"], gaussian_filter(dss['prmsl'].values, sigma=1) * units(elem_units[3]))
 
