2e9c2571e7e37a7829eea58a544c71efa1dfbe9c
Horita <ryohorita105@gmail.com>
2024-03-17 14:00:01 +0900

    Update test.py

--- test.py
+++ test.py
@@ -146,10 +146,10 @@ w = gaussian_filter(np.sqrt(dss['10u'].values ** 2 + dss['10v'].values ** 2), si
 # wが5以下の場所のみフィルタリング
 #dss['prmsl'] = (["lat", "lon"], np.where(w <= 5, gaussian_filter(dss['prmsl'].values, sigma=4), dss['prmsl'].values) * units(elem_units[3]))
 #dss['prmsl'] = (["lat", "lon"], np.where(w <= 15, gaussian_filter(dss['prmsl'].values, sigma=2), dss['prmsl'].values) * units(elem_units[3]))
-#dss['prmsl'] = (["lat", "lon"], cv2.bilateralFilter(dss['prmsl'].values, d=1000, sigmaColor=1, sigmaSpace=1) * units(elem_units[3]))
+#dss['prmsl'] = (["lat", "lon"], cv2.bilateralFilter(dss['prmsl'].values, d=1, sigmaColor=1, sigmaSpace=1) * units(elem_units[3]))
 
 pressure_data_uint8 = ((dss['prmsl'].values - dss['prmsl'].values.min()) / (dss['prmsl'].values.max() - dss['prmsl'].values.min()) * 255).astype('uint8')
-filtered_pressure_data = cv2.bilateralFilter(pressure_data_uint8, d=100, sigmaColor=2, sigmaSpace=2)
+filtered_pressure_data = cv2.bilateralFilter(pressure_data_uint8, d=1, sigmaColor=2, sigmaSpace=2)
 dss['prmsl'] = (["lat", "lon"], ((filtered_pressure_data / 255) * (dss['prmsl'].values.max() - dss['prmsl'].values.min()) + dss['prmsl'].values.min()) * units(elem_units[3]))
 
 #dss['prmsl'] = (["lat", "lon"], gaussian_filter(dss['prmsl'].values, sigma=1) * units(elem_units[3]))
