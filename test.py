a842d004dbfa133b94da3d5efc3827fca8cb18f8
Horita <ryohorita105@gmail.com>
2024-05-09 10:56:50 +0900

    Update test.py

--- test.py
+++ test.py
@@ -155,8 +155,8 @@ surf = gaussian_filter(surf, sigma=1)
 #dss['prmsl'] = (["lat", "lon"], np.where(w <= 15, gaussian_filter(dss['prmsl'].values, sigma=1), dss['prmsl'].values) * units(elem_units[3]))
 #dss['prmsl'] = (["lat", "lon"], gaussian_filter(dss['prmsl'].values, sigma=1) * units(elem_units[3]))
 #dss['prmsl'] = (["lat", "lon"], gaussian_filter(dss['prmsl'].values, sigma=4-np.sqrt(w)) * units(elem_units[3]))
-sigma = 10 - np.sqrt(w)
-dss['prmsl'] = (["lat", "lon"], gaussian_filter(dss['prmsl'].values, sigma=sigma, mode='constant', truncate=3.0) * units(elem_units[3]))
+#sigma = 10 - np.sqrt(w)
+dss['prmsl'] = (["lat", "lon"], gaussian_filter(dss['prmsl'].values, sigma=dss['prmsl'].values/1000.0, mode='constant', truncate=3.0) * units(elem_units[3]))
 ## 読み込むの高度上限の指定：tagLpより下層の等圧面データをXarray Dataset化する
 tagLp = 300
 
