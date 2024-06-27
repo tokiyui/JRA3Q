cd0b58cb5ea5dc3cd4858ba3ec7bc952deb621d8
Horita <ryohorita105@gmail.com>
2024-06-27 12:43:27 +0900

    Update test.py

--- test.py
+++ test.py
@@ -390,7 +390,7 @@ for i in range(len(minid[0])):
 
 # 500hPa 等高度線                                                                                                      
 ax.contourf(ds4['lon'], ds4['lat'], ds4['hgt'].sel(level=500.0), levels=np.arange(4980, 6000, 60), cmap='turbo', transform=latlon_proj, extend='both', alpha=0.5)
-z500 = ax.contour(ds4['lon'], ds4['lat'], ds4['hgt'].sel(level=500.0), levels=np.arange(4980, 6000, 60), colors='white', linewidths=3.0, linestyles='solid', transform=latlon_proj, alpha=0)
+z500 = ax.contour(ds4['lon'], ds4['lat'], ds4['hgt'].sel(level=500.0), levels=np.arange(4980, 6000, 60), colors='black', linewidths=1.0, linestyles='solid', transform=latlon_proj)
 ax.clabel(z500, z500.levels, fontsize=11, inline=True, inline_spacing=1, fmt='%i', rightside_up=True, colors='black', alpha=1)
 
 # 850hPa 等温線
